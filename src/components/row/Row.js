b04299f31a6e39b923ccd957308be4bd7cb0e0e1
Marty Siksnis <siksnis@Martys-MacBook-Pro.local>
2020-10-12 23:34:38 +0200

    Added functionality when pressed on a movie thumbnail it will show a trailer from youtube. Used react-youtube and movie-trailer packeges.

--- src/components/row/Row.js
+++ src/components/row/Row.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import "./Row.css";
 import axios from "../../axios";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     async function fetchData() {
@@ -18,6 +21,28 @@ function Row({ title, fetchUrl, isLargeRow }) {
 
   // console.log(movies);
 
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters/
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
   return (
     <div className="row">
       <h2>{title}</h2>
@@ -26,6 +51,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
           <img
             className={`row__poster ${isLargeRow && "row__posterLarge"}`}
             key={movie.id}
+            onClick={() => handleClick(movie)}
             src={`${base_url}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
             }`}
@@ -33,6 +59,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
           />
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
