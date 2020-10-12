dbf2d80f9817c8dfc0889039a68194bd83ac79cd
Marty Siksnis <siksnis@Martys-MacBook-Pro.local>
2020-10-12 23:05:06 +0200

    Made navbar with logo and avatar and background which comes when scrolled down a bit.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import Banner from "./components/banner/Banner";
+import Nav from "./components/navbar/Nav";
 import Row from "./components/row/Row";
 import requests from "./requests";
 
 function App() {
   return (
     <div className="app">
+      <Nav />
       <Banner />
       <Row
         title="Netflix Originals"
